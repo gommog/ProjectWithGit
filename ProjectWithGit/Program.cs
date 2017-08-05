9e7da1cc9da87ea94d0fdef8e154df7550b8da0a
gommog <gom.mog@mail.ee>
2017-08-05 15:41:08 +0300

    Let try this

---
+++ ProjectWithGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi, let's restarted with Git and Control");
+            Console.ReadLine();
+
+        }
+    }
+}
